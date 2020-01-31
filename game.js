0523145520407737d0f3a0f7d003cafaea82506c
EC2 Default User <ec2-user@ip-172-31-39-189.us-east-2.compute.internal>
2020-01-31 19:54:52 +0000

    Refactored

--- game.js
+++ game.js
@@ -5,52 +5,119 @@ var render = Matter.Render.create({
   canvas: myCanvas,
   engine: engine,
   options: {
-    width: 1400,
-    height: 600,
-    background: '000000',
+    width: 1200,
+    height: 800,
+    background: 'space.png',
     wireframes: false,
     showAngleIndicator: true
   }
 });
 
-var ball = Matter.Bodies.circle(600, 400, 30, {
-  density: 0.04,
-  friction: 0.01,
-  frictionAir: 0.00001,
-  restitution: 0.8,
-  render: {
-    fillStyle: '#F35e66',
-    strokeStyle: 'black',
-    lineWidth: 1,
-  }
-});
+engine.timing.timeScale = 0.8;
 
-var player = Matter.Bodies.circle(540, 400, 40, {
-  density: 0.001,
-  friction: 0.7,
-  frictionStatic: 0,
-  frictionAir: 0.01,
-  restitution: 0.5,
-  ground: false,
-  render: {
-    fillStyle: '#000000',
-    strokeStyle: 'black',
-    lineWidth: 1,
-    sprite: {
-      texture: 'alien.png',
+//Game classes below
+class Projectile{
+  constructor(x,y,r){
+    this.dimensions = Matter.Bodies.circle(x, y, r, {
+      density: 0.04,
+      friction: 0.01,
+      frictionAir: 0.00001,
+      restitution: 0.8,
+      render: {
+        fillStyle: '#F35e66',
+        strokeStyle: 'black',
+        lineWidth: 1,
+      }
+    });
+  }
+}
+
+class Character{
+  constructor(x,y,w,h,texture){
+    this.dimensions = Matter.Bodies.rectangle(x, y, w, h,{
+        density: 0.01,
+        friction: 0.7,
+        frictionStatic: 0,
+        frictionAir: 0.01,
+        restitution: 0.5,
+        ground: false,
+        render: {
+          fillStyle: '#000000',
+          strokeStyle: 'black',
+          lineWidth: 1,
+          sprite: {
+            texture: texture,
+            xScale: 0.3,
+            yScale: 0.3
+          }
+        }
+    });
+  }
+}
+
+class Fodder{
+  constructor(x,y,w,h,texture){
+    this.dimensions = Matter.Bodies.rectangle(x, y, w, h,{
+      density: 0.1,
+      friction: 0.7,
+      frictionStatic: 0,
+      frictionAir: 0.01,
+      restitution: 0.5,
+      ground: false,
+      render: {
+        fillStyle: '#000000',
+        strokeStyle: 'black',
+        lineWidth: 1,
+        sprite: {
+          texture: texture,
+          xScale: 0.3,
+          yScale: 0.3
+        }
+      }
+    });
+  }
+}
+
+class SlingShot {
+  constructor(x,y,body){
+    const options = {
+      pointA: {
+        x:x,
+        y:y
+      },
+      bodyB: body,
+      stiffness: 0.02,
+      length: 20,
+      render: {
+        fillStyle: '#000000',
+        strokeStyle: 'black',
+        lineWidth: 1,
+      }
     }
+    this.sling = Matter.Constraint.create(options)
+    Matter.World.add(world, this.sling)
   }
-});
+  
+  fly(){
+    this.sling.bodyB = null
+    this.sling.pointA = null
+  }
+}
 
-Matter.World.add(world, [ball, player]);
 
-var keyPresses = {};
-document.body.addEventListener("keydown", function(e) {
-  keyPresses[e.keyCode] = true;
-});
-document.body.addEventListener("keyup", function(e) {
-  keyPresses[e.keyCode] = false;
-});
+var ball = new Projectile(200, 400, 30).dimensions
+var slingShot = new SlingShot(200, 400, ball)
+var player = new Character(700, 400, 40, 120, 'alien.png').dimensions
+
+Matter.World.add(world, [
+  ball, 
+  player, 
+  new Fodder(600, 400, 77, 77, 'crate.png').dimensions, 
+  new Fodder(600, 400, 77, 77, 'crate.png').dimensions,
+  new Fodder(600, 400, 77, 77, 'crate.png').dimensions, 
+  new Fodder(600, 400, 77, 77, 'crate.png').dimensions]);
+
+
 
 var mouseConstraint = Matter.MouseConstraint.create(engine, { //Create Constraint
   element: myCanvas,
@@ -58,34 +125,54 @@ var mouseConstraint = Matter.MouseConstraint.create(engine, { //Create Constrain
     render: {
       visible: true
     },
-    stiffness:0.8
+    stiffness:0.5
   }
 });
+
 mouseConstraint.mouse.element.removeEventListener("mousewheel", mouseConstraint.mouse.mousewheel);
 mouseConstraint.mouse.element.removeEventListener("DOMMouseScroll", mouseConstraint.mouse.mousewheel);
 Matter.World.add(world, mouseConstraint);
 
-var ceiling = Matter.Bodies.rectangle(800, 0, 1400, 80, {
+//Allows the ball to be released 1 second after the user releases the mouse
+myCanvas.addEventListener('mouseup', function(){
+  setTimeout(() => {
+    slingShot.fly() 
+  }, 100)
+})
+
+//Create 4 walls to play the game within.
+var ceiling = Matter.Bodies.rectangle(700, 0, 1400, 80, {
   isStatic: true,
   render: {
     visible: true
   }
 });
 
-var floor = Matter.Bodies.rectangle(800, 600, 1400, 80, {
+var floor = Matter.Bodies.rectangle(700, 800, 1400, 80, {
   isStatic: true,
   render: {
     visible: true
   }
 });
 
-var left_wall = Matter.Bodies.rectangle(400, 600, 800, 80, {
+var left_wall = Matter.Bodies.rectangle(20, 400, 40, 720, {
   isStatic: true,
   render: {
     visible: true
   }
 });
-Matter.World.add(world, [floor, ceiling]);
+
+var right_wall = Matter.Bodies.rectangle(1180, 400, 40, 720, {
+  isStatic: true,
+  render: {
+    visible: true
+  }
+});
+
+//Add gameplay area to world
+Matter.World.add(world, [floor, ceiling, left_wall, right_wall]);
+
+
 
 Matter.Engine.run(engine);
 Matter.Render.run(render);
